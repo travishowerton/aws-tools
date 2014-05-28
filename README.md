fe3d32db73095d071919a87e7e9272a4798701d0
Kevin F Connors <kevin_connors@pharmcomp.com>
2014-05-27 17:22:51 -0700

    Updated docs.  Fixed put_metric and put_file_to_bucket

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ have at it.
 Fair Warning
 ------------
 The current version 0.0.2 is not really intended for general use.  You will find the
-library incomplete and lightly tested.  In addition, we expect that the API will 
+library incomplete and lightly tested.  In addition, we expect that the API will
 change as development continues.
 
 Installation
@@ -21,8 +21,214 @@ The aws-tools gem can be installed by running:
     gem install aws-tools
 
 
-Command Line Example Usage
---------------------------
+API Example Usage
+-----------------
+
+Documentation
+-------------
+
+API documentation for aws-tools is available at: [RubyDoc.info](http://rubydoc.info/github/greenkarmet/aws-tools/master/frames)
+
+
+License
+-------
+
+aws-tools is released under the MIT license, see LICENSE for details.
+
+
+Source Code
+-----------
+
+Source code for aws-tools is available on [GitHub](https://github.com/greenkarmet/aws-tools).
+
+
+Issue Tracker
+-------------
+
+Please post any bugs, issues, feature requests or questions to the
+[GitHub issue tracker](https://github.com/greenkarmet/aws-tools/issues).
+
+Command Line
+============
+
+Command Line Syntax
+-------------------
+
+Many of the commands require specifying the region, environment and purpose of the object being managed, the region
+can be any one of **[or, ca, va]**.  The environment and purpose are tags on the object that allow objects to be organized
+loosly within groups.  In practice, we normally use enviornments of **'test'**, **'demo'**, **'development'**, or **'production'** and
+purpose is much more flexible and more aligned with the use of the object such as **'app'**, **'db'**, **'mongo'**, etc.
+
+A subset of these commands allow specifying a method of selecting an instance based on age using the **--choose** flag.
+With this flag, it is possible to select the **first** instance found, the **oldest** instance or the **newest** instance.
+
+The commands start and terminate_instance allow the flag **--keep-one** which guarantees that the aws_manager.rb will not remove
+the last instance matching the criteria.
+
+###EC2 Instance commands:
+
+        aws_manager.rb --region region run_instance <instance template file>
+        aws_manager.rb --region region [--environment environment] [--purpose purpose] [--choose first|oldest|newest] connect [id]
+        aws_manager.rb --region region [--environment environment] [--purpose purpose] [--choose first|oldest|newest] start [id]
+        aws_manager.rb --region region [--environment environment] [--purpose purpose] [--choose first|oldest|newest] [--keep-one] stop [id]
+        aws_manager.rb --region region [--environment environment] [--purpose purpose] [--choose first|oldest|newest] get_instance_status [id]
+        aws_manager.rb --region region [--environment environment] [--purpose purpose] [--choose first|oldest|newest] get_instance_ip [id]
+        aws_manager.rb --region region [--environment environment] [--purpose purpose] [--choose first|oldest|newest] [--keep_one] terminate_instance [id]
+
+####run_instance
+
+This command builds a new instance based on the criteria in the YAML template.  This template is of the format:
+
+        ---
+        :tags:
+          :environment: test
+          :purpose: app
+          :elastic_lb: TestAppELB
+          :Name: TestappServer
+          :user: ubuntu
+          :security_group_ids: sg-ffffffff
+          :subnet_id: subnet-fffffffff
+          :elastic_ip_allocation_id: eipalloc-fffffffff
+        :template:
+          :image_id: ami-fffffffff
+          :instance_type: m1.small
+          :max_count: 1
+          :min_count: 1
+          :monitoring:
+            :enabled: false
+          :network_interfaces:
+          - :associate_public_ip_address: true
+            :description: Primary
+            :subnet_id: subnet-fffffffff
+            :delete_on_termination: true
+            :device_index: 0
+          :placement:
+            :availability_zone: us-west-2a
+            :tenancy: default
+
+#####template
+
+These are parameters passed directly to the AWS instance run_instance facility.  See [AWA Run Instance Documentation](http://docs.aws.amazon.com/sdkforruby/api/frames.html)
+
+#####tags
+
+Anything in the list of tags will be applied to the instance after it is created.  The specific tags listed
+above also help direct the aws_manager.rb when the instance is started in the future.  These are:
+
+ * **elastic_ip** - The elastic IP to bind to the instance when it starts
+ * **security_group_ids** - Comma separated list of security group ids to bind the instance to when it starts
+ * **subnet_id** - Subnet id to put the instance in
+ * **elastic_ip_allocation_id** - If instance is in a vpc, the id of the elastic ip to bind the instance to
+
+####connect
+
+Connects to a specific instance using ssh
+
+####start
+
+Starts an instance
+
+####stop
+
+Stops an instance
+
+####get_instance_status
+
+Reports on the current status of an instance
+
+####get_instance_ip
+
+Reports the current public IP of an instance
+
+####terminate_instance
+
+Terminates an instance
+
+###CW commands:
+
+        aws_manager.rb --region region put_cw_metric csv_metric
+
+Sends a metric to cloud watch.  The metric must be formatted as a csv row of the format **"namespace,name,value,dims"**
+
+ * Note that dims is formatted as an arbitrary semicolon separated list of name:value dimensions.  For example:
+
+        activeservers,count,10,env:prod;purp:test
+
+###S3 commands:
+
+        aws_manager.rb --region region put_to_bucket filename s3_filename
+
+Put a local file (filename) to an s3 bucket defined by s3_filename.  The S3 filename must be of the format: bucketname:/s3/file/path
+
+###RDS commands:
+
+        aws_manager.rb --region region create_db <db template file>
+        aws_manager.rb --region region [--environment environment] [--purpose purpose] delete_db [id]
+        aws_manager.rb --region region [--environment environment] [--purpose purpose] wait_for_db [id]
+        aws_manager.rb --region region [--environment environment] [--purpose purpose] get_db_status [id]
+        aws_manager.rb --region region [--environment environment] [--purpose purpose] get_db_endpoint [id]
+        aws_manager.rb --region region [--environment environment] [--purpose purpose] purge_db_snapshots [id]
+
+####run_instance
+
+This command builds a new instance based on the criteria in the YAML template.  This template is of the format:
+
+        ---
+        :tags:
+          :environment: demo
+          :purpose: db
+          :name: mazamademo
+          :snapshot_name: mazamademo
+          :vpc_security_group_ids: sg-24010b46
+        :opts:
+          :db_instance_identifier: mazamademo
+          :db_subnet_group_name: testdbsubnetgroup
+          :publicly_accessible: false
+          :db_instance_class: db.t1.micro
+          :availability_zone: us-west-2a
+          :multi_az: false
+          :engine: mysql
+
+####tags
+
+Anything in the list of tags will be applied to the instance after it is created.  In addition, there
+are some required tags.  These are:
+
+ * **name** - Database name
+ * **snapshot_name** - Basename of the snapshot to use when building the database
+ * **vpc_security_group_ids** - Comma-separated list of vpc_security_group_ids to be bound to instance
+
+####opts
+
+Parameters passed directly to RDS restore_db_instance_from_db_snapshot method.  See [RDS Restore instance from db method](http://docs.aws.amazon.com/sdkforruby/api/Aws/RDS/V20130909.html#restore_db_instance_from_db_snapshot-instance_method)
+
+
+###SNS commands:
+
+        aws_manager.rb --region region sns \"<topic_arn>\" \"subject\"
+
+example topic_arn:
+
+        arn:aws:sns:us-east-1:795987318935:prod_app_start_failure
+
+###Other commands:
+
+        aws_manager.rb --help
+
+###Misc
+
+        Note that there are shortened versions of the options flags:
+          --region      = -r
+          --environment = -e
+          --purpose     = -p
+          --choose      = -c
+          --keep_one    = -k
+          --help        = -h
+
+
+Command Line Example
+--------------------
+
 
 Creating an instance is a feature we use heavily to scale our app as needed.  To create
 an EC2 instance, it is necessary to have a instance template in the form of yaml located
@@ -73,29 +279,4 @@ With these files installed, the following command would build this new instance:
 
     aws_manager.rb -r or -e test -p app run_instance /etc/app_server.yaml
 
-API Example Usage
------------------
 
-Documentation
--------------
-
-API documentation for aws-tools is available at: [RubyDoc.info](http://rubydoc.info/github/greenkarmet/aws-tools/master/frames)
-
-
-License
--------
-
-aws-tools is released under the MIT license, see LICENSE for details.
-
-
-Source Code
------------
-
-Source code for aws-tools is available on [GitHub](https://github.com/greenkarmet/aws-tools).
-
-
-Issue Tracker
--------------
-
-Please post any bugs, issues, feature requests or questions to the 
-[GitHub issue tracker](https://github.com/greenkarmet/aws-tools/issues).
--- bin/aws_manager.rb
+++ bin/aws_manager.rb
@@ -2,8 +2,9 @@
 
 require 'aws-sdk-core'
 require 'yaml'
-require 'json'
-require './lib/aws_region.rb'
+require 'optparse'
+require 'erb'
+require_relative 'lib/aws_region.rb'
 
 VALID_COMMANDS=['start', 
                 'stop', 
@@ -19,10 +20,11 @@ VALID_COMMANDS=['start',
                 'purge_db_snapshots', 
                 'put_cw_metric', 
                 'put_to_bucket', 
-                'run_instance', 
+                'run_instance',
+                'sns',
                 'terminate_instance']
 
-def locate_instance_choice(all_instances, instance_id, statuses)
+def filter_instances(all_instances, statuses)
   instances = []
   if statuses.length == 0
     instances = all_instances
@@ -33,8 +35,12 @@ def locate_instance_choice(all_instances, instance_id, statuses)
       end
     end
   end
+  instances
+end
+def locate_instance_choice(instances, instance_id, options={})
+  selection_filter = options.has_key?(:selection_filter) ? options[:selection_filter] : nil
   if instances.length == 0
-    puts "There are no running instances matching criteria"
+    puts "There are no instances matching criteria"
     return nil
   elsif instances.length == 1
     instances[0]
@@ -43,7 +49,7 @@ def locate_instance_choice(all_instances, instance_id, statuses)
       instance = nil
       if instance_id.length > 2 # This is an instance id
         instances.each do |i|
-          if i[:instance_id] == instance_id
+          if i.id == instance_id
             instance = i
           end
         end
@@ -53,137 +59,163 @@ def locate_instance_choice(all_instances, instance_id, statuses)
         end
       else
         if instances.length - 1 >= instance_id.to_i
-          instance = running_instances[instance_id.to_i]
+          instance = instances[instance_id.to_i]
         end
       end
       return instance
     else
-      puts "Error, there are multiple instances that match these tags.  Please choose one:"
-      puts "index: private_ip / public_ip / instance_id / launch_time"
-      for i in 0..(instances.length - 1)
-        puts "#{i}: #{instances[i][:private_ip_address]} / #{instances[i][:public_ip_address]} / #{instances[i][:instance_id]} / #{instances[i][:launch_time]}"
+      if selection_filter == :first
+        return instances[0]
+      elsif selection_filter == :oldest
+          oldest = instances[0]
+          instances.each do |i|
+            oldest = i if i._instance[:launch_time] < oldest._instance[:launch_time]
+          end
+          return oldest
+      elsif selection_filter == :newest
+        newest = instances[0]
+        instances.each do |i|
+          newest = i if i._instance[:launch_time] > newest._instance[:launch_time]
+        end
+        return newest
+      else
+        puts "Error, there are multiple instances that match these tags.  Please choose one:"
+        puts "index: private_ip / public_ip / instance_id / launch_time"
+        for i in 0..(instances.length - 1)
+          puts "#{i}: #{instances[i]._instance[:private_ip_address]} / #{instances[i]._instance[:public_ip_address]} / #{instances[i]._instance[:instance_id]} / #{instances[i]._instance[:launch_time]}"
+        end
+        ans = STDIN.gets.chomp().to_i
+        return instances[ans]
       end
-      ans = STDIN.gets.chomp().to_i
-      return instances[ans]
     end
     nil
   end
 
 end
 def syntax
-  puts "Invalid command.\n" +
-        "Syntax: aws_manager [or|ca|va] " + 
-          "[prod|test|..etc] " +
-          "[app|hub|..etc] " +
-          "[#{VALID_COMMANDS.join("|")} " +
-          "{name}"
+  puts "Syntax:"
+  puts "   EC2 Instance commands:"
+  puts "        aws_manager.rb --region region run_instance <instance template file> "
+  puts "        aws_manager.rb --region region [--environment environment] [--purpose purpose] [--choose first|oldest|newest] connect [id]"
+  puts "        aws_manager.rb --region region [--environment environment] [--purpose purpose] [--choose first|oldest|newest] start [id]"
+  puts "        aws_manager.rb --region region [--environment environment] [--purpose purpose] [--choose first|oldest|newest] [--keep-one] stop [id]"
+  puts "        aws_manager.rb --region region [--environment environment] [--purpose purpose] [--choose first|oldest|newest] get_instance_status [id]"
+  puts "        aws_manager.rb --region region [--environment environment] [--purpose purpose] [--choose first|oldest|newest] get_instance_ip [id]"
+  puts "        aws_manager.rb --region region [--environment environment] [--purpose purpose] [--choose first|oldest|newest] [--keep_one] terminate_instance [id]"
+  puts "   CW commands:"
+  puts "        aws_manager.rb --region region put_cw_metric csv_metric"
+  puts "   S3 commands:"
+  puts "        aws_manager.rb --region region put_to_bucket filename s3_filename"
+  puts "   RDS commands:"
+  puts "        aws_manager.rb --region region create_db <db template file> "
+  puts "        aws_manager.rb --region region [--environment environment] [--purpose purpose] delete_db [id]"
+  puts "        aws_manager.rb --region region [--environment environment] [--purpose purpose] wait_for_db [id]"
+  puts "        aws_manager.rb --region region [--environment environment] [--purpose purpose] get_db_status [id]"
+  puts "        aws_manager.rb --region region [--environment environment] [--purpose purpose] get_db_endpoint [id]"
+  puts "        aws_manager.rb --region region [--environment environment] [--purpose purpose] purge_db_snapshots [id]"
+  puts "   SNS commands:"
+  puts "        aws_manager.rb --region region sns \"<topic_arn>\" \"subject\""
+  puts "        example topic_arn: 'arn:aws:sns:us-east-1:795987318935:prod_app_start_failure'"
+  puts "   Other commands:"
+  puts "        aws_manager.rb --help"
+  puts "\nNote that there are shortened versions of the options flags:"
+  puts "        --region      = -r"
+  puts "        --environment = -e"
+  puts "        --purpose     = -p"
+  puts "        --choose      = -c"
+  puts "        --keep_one    = -k"
+  puts "        --help        = -h"
   exit
 end
 def main
-  name = nil
-  if ARGV.length == 3 and ARGV[1].downcase == "put_cw_metric"
-      region  = ARGV[0].downcase
-      command = ARGV[1].downcase
-      metric  = ARGV[2].downcase
-  elsif ARGV.length == 5 and ARGV[1].downcase == "put_to_bucket"
-    region         = ARGV[0].downcase
-    command        = ARGV[1].downcase
-    bucket         = ARGV[2].downcase
-    filename       = ARGV[3].downcase
-    file_identity  = ARGV[4].downcase
-  else
-    if ARGV.length < 4
-      syntax
-    end
-    region = ARGV[0].downcase
-    instance_env = ARGV[1].downcase
-    instance_purpose = ARGV[2].downcase
-    command = ARGV[3].downcase
-    name = ARGV[4].downcase if ARGV.length == 5
-  end
-  syntax if !(['or', 'ca', 'va'].include?(region))
+  syntax if ARGV.length <= 0
+  params = ARGV.getopts("hr:e:p:fkc:", "choose:", "keep-one", "help", "region:", "environment:", "purpose:")
+  syntax if params['h'] or params['help']
+  purpose = params['p'] || params['purpose']
+  environment = params['e'] || params['environment']
+  region = params['r'] || params['region']
+  keep_one = params['k'] || params['keep-one']
+  selection_criteria = params['c'] || params['choose']
+  selection_criteria = selection_criteria.downcase.to_sym if selection_criteria
+  syntax if selection_criteria and !([:first,:oldest, :newest].include?(selection_criteria))
+  syntax if ARGV.length <= 0
+  command = ARGV.shift
+  save_cl = ARGV.dup
+  name = ARGV.length == 1 ? ARGV.shift : nil
+
+  syntax if !region or !(['or', 'ca', 'va'].include?(region))
   syntax if !(VALID_COMMANDS.include?(command))
   
   cfg = YAML::load File.read("/etc/auth.yaml")
   region = AwsRegion.new(region, cfg["account_id"], cfg["access_key_id"], cfg["secret_access_key"])
 
   if command == 'stop'
-
-    instance = locate_instance_choice(region.find_instances({environment: instance_env, purpose: instance_purpose  }),
-                                      name,
-                                      ['running'])
+    if keep_one and filter_instances(region.find_instances({environment: environment, purpose: purpose  }), ['running']).length < 2
+      puts "Error, there are less than 2 instances, and keep_one flag set.  Exiting."
+      exit
+    end
+    instance = locate_instance_choice(filter_instances(region.find_instances({environment: environment, purpose: purpose  }), ['running']),name, {:selection_filter => selection_criteria})
     instance.stop(wait=true)
   elsif command == 'start'
-    instance = locate_instance_choice(region.find_instances({environment: instance_env, purpose: instance_purpose  }),
-                                      name,
-                                      ['stopped'])
+    instance = locate_instance_choice(filter_instances(region.find_instances({environment: environment, purpose: purpose  }), ['stopped']),name, {:selection_filter => selection_criteria})
     instance.start(wait=true)
   elsif command == 'connect'
-    instance = locate_instance_choice(region.find_instances({environment: instance_env, purpose: instance_purpose  }),
-                                      name,
-                                      ['running'])
+    instance = locate_instance_choice(filter_instances(region.find_instances({environment: environment, purpose: purpose  }), ['running']),name, {:selection_filter => selection_criteria})
     instance.connect
   elsif command == 'create_db'
-    options = YAML::Load name if File.exists? name
+    options = YAML::load File.read(name) if File.exists? name
     instance = region.create_db_instance(options)
   elsif command == 'delete_db'
-    instances = region.find_db_instances({:environment => instance_env, :purpose => instance_purpose, :instance_id => name})
+    instances = region.find_db_instances({:environment => environment, :purpose => purpose, :instance_id => name})
     instances[0].delete
   elsif command == 'wait_for_db'
-    instances = region.find_db_instances({:environment => instance_env, :purpose => instance_purpose, :instance_id => name})
+    instances = region.find_db_instances({:environment => environment, :purpose => purpose, :instance_id => name})
     instances[0].wait
   elsif command == 'get_db_endpoint'
-    instances = region.find_db_instances({:environment => instance_env, :purpose => instance_purpose, :instance_id => name})
+    instances = region.find_db_instances({:environment => environment, :purpose => purpose, :instance_id => name})
     puts instances[0].endpoint
   elsif command == 'get_db_status'
-    instances = region.find_db_instances({:environment => instance_env, :purpose => instance_purpose, :instance_id => name})
+    instances = region.find_db_instances({:environment => environment, :purpose => purpose, :instance_id => name})
     puts instances[0].status
   elsif command == 'get_instance_status'
-    instance = locate_instance_choice(region.find_instances({environment: instance_env, purpose: instance_purpose  }),
-                                      name,
-                                      [])
+    instance = locate_instance_choice(filter_instances(region.find_instances({environment: environment, purpose: purpose  }), []), name, {:selection_filter => selection_criteria})
     puts instance.state
   elsif command == 'get_instance_ip'
-    instance = locate_instance_choice(region.find_instances({environment: instance_env, purpose: instance_purpose  }),
-                                      name,
-                                      [])
+    instance = locate_instance_choice(filter_instances(region.find_instances({environment: environment, purpose: purpose  }), []), name, {:selection_filter => selection_criteria})
     puts instance.public_ip
   elsif command == 'purge_db_snapshots'
-    instances = region.find_db_instances({:environment => instance_env, :purpose => instance_purpose, :instance_id => name})
+    instances = region.find_db_instances({:environment => environment, :purpose => purpose, :instance_id => name})
     instances[0].purge_db_snapshots
   elsif command == 'put_cw_metric'
+    syntax if ARGV.length <= 1
     instance = region.create_cw_instance
-    instance.put_metric(metric)
+    instance.put_metric(name)
   elsif command == 'put_to_bucket'
+    syntax if ARGV.length <= 0
+    (bucket,file_identity) = ARGV[0].split(/:/)
+    syntax if bucket.strip.length <=0 or file_identity.strip.length <= 0
     instance = region.find_bucket({bucket: bucket})
-    instance.put_file(filename, file_identity)
+    instance.put_file(name, file_identity)
   elsif command == 'run_instance'
     instance_template = name
     if !File.exists?(instance_template)
       puts "Cannot find instance template to build server from: #{instance_template}"
       exit
     end
-    image_options = YAML:: File.read(instance_template)
-    instance = region.create_instance(image_options[:template])
-    tags = {:environment => instance_env,
-            :purpose     => instance_purpose,
-            "Name"       => image_options[:name],
-            :user        => image_options[:user]
-           }
-    tags[:elastic_lb] = image_options[:elastic_lb] if image_options.has_key?(:elastic_lb)
-    instance.add_tags(tags)
-    if image_options.has_key?(:security_group_ids)
-      instance.set_security_groups(image_options[:security_group_ids])
-    end
-    if image_options.has_key?(:elb)
-      instance.add_to_lb(image_options[:elb])
-    end
-
+    #image_options = YAML::load File.read(instance_template)
+    image_options = YAML.load(ERB.new(File.read(instance_template)).result)
+    instance = region.create_instance(image_options)
   elsif command == 'terminate_instance'
-    instance = locate_instance_choice(region.find_instances({environment: instance_env, purpose: instance_purpose  }),
-                                      name,
-                                      [])
+    if keep_one and filter_instances(region.find_instances({environment: environment, purpose: purpose  }), ['running']).length < 2
+      puts "Error, there are less than 2 instances, and keep_one flag set.  Exiting."
+      exit
+    end
+    instance = locate_instance_choice(filter_instances(region.find_instances({environment: environment, purpose: purpose  }), []), name, {:selection_filter => selection_criteria})
     instance.terminate
+  elsif command == 'sns'
+    instance = region.create_sns_instance
+    instance.publish save_cl[0], save_cl[1]
+    exit
   elsif
     syntax
   end
